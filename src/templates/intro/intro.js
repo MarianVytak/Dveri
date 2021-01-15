c2d14abbac091ccd5e5d0400a68d52e9d846b8ba
MarianVytak <marianvytak@gmail.com>
2021-01-15 22:30:22 +0200

    First Commit

---
+++ src/templates/intro/intro.js
@@ -0,0 +1,21 @@
+$(function () {
+
+    'use strict';
+
+
+
+    $('.intro__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        focusOnSelect: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        arrows: true,
+        dots: true,
+    });
+
+
+
+});
